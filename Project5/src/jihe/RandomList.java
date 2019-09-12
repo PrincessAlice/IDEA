214a9fdf24a05541b3a11be48940371f67981678
PrincessAlice <1337180988@qq.com>
2019-09-12 23:38:33 +0800

    提交代码

--- Project5/src/jihe/RandomList.java
+++ Project5/src/jihe/RandomList.java
@@ -1,13 +1,16 @@
 package jihe;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RandomList {
     public static  class Node{
         int val;
         Node next;
         Node random;
     }
-    public Node copy1(Node head){
+    public static Node copy1(Node head){
         Node p = head;
         while(p != null){
             Node q = new Node();
@@ -20,10 +23,44 @@ public class RandomList {
         }
         p = head;
         while(p != null){
-
+            Node q = p.next;
+            if(p.random != null){
+                q.random = p.random.next;
+            }else{
+                q.random = null;
+            }
+            p = q.next;
         }
+        //把两个链表拆开
+        return head;
+    }
 
-        return null;
+    public static Node copy2(Node head){
+        Node p = head;
+        Node result = null;
+        Node last = null;
+        Map<Node,Node>map = new HashMap<>();
+        while(p != null){
+            Node q = new Node();
+            q.val = p.val;
+            if(result == null){
+                result = q;
+            }else{
+                last.next = q;
+            }
+
+            last = q;
+            map.put(p,q);
+            p = p.next;
+        }
+        p = head;
+        Node q = result;
+        while(p != null){
+            q.random = map.get(p.random);
+            p = p.next;
+            q = q.next;
+        }
+        return result;
     }
 
 }
