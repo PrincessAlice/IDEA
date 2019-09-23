ff08a4bece29bd05108e133af14cdcd3514478bd
PrincessAlice <1337180988@qq.com>
2019-09-23 23:32:55 +0800

    提交

---
+++ TestC1/c4.c
@@ -0,0 +1,21 @@
+/* Note:Your choice is C IDE */
+#include "stdio.h"
+#define N 20;
+#define M 5；
+float score[N][M];
+float a_stu[N],a_cour[M];
+int main()
+{int i,j;
+	float max();                                                          //函数声明
+	float min();                                                          //函数声明
+void fail(char course[N][M],int num[],float*pscore,float aver[N]);        //函数声明
+void input_stu(void);
+void aver_stu(void);
+void aver_cour(void);
+input_stu();
+aver_stu();
+aver_cour();
+printf()
+	
+    
+}
