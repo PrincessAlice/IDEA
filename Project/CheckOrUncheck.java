28d0cf6c45c30f9b824e1bbfdae45455ea392dcd
PrincessAlice <1337180988@qq.com>
2019-09-29 22:38:22 +0800

    提交

---
+++ Project/CheckOrUncheck.java
@@ -0,0 +1,29 @@
+package Exception异常;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CheckOrUncheck {
+    public static void methodA(){
+        throw new ArrayIndexOutOfBoundsException();
+    }
+
+    public static void methodB() throws IOException{
+        throw new IOException();
+    }
+
+    public static void methodC() throws IOException{
+        methodB();
+    }
+
+    public static void tryResource(){
+        try(Scanner scanner = new Scanner(System.in)){
+            scanner.nextInt();
+        }
+    }
+
+    public static void methodUncheck() throws RuntimeException{
+        throw new RuntimeException();
+    }
+
+}
