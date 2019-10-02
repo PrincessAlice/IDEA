27d6d7ea3811b5334b9d6ea4e6267b00ebddc04b
PrincessAlice <1337180988@qq.com>
2019-10-02 21:48:48 +0800

    提交

---
+++ BITE/SeqList.java
@@ -0,0 +1,51 @@
+//Sequence List
+//顺序表中保存的数据元素类型是int
+
+public class SeqList{
+	//属性
+	
+	//构造方法
+	public SeqList(){
+		
+	}
+	
+	//支持的方法
+	//头插O(n)
+	public viod pushFront(int element){
+		
+	}
+	
+	//尾插 O(1)
+	public void pushBack(int element){
+		
+	}
+	
+	//中间插入，根据下标做插入 O(n)
+	public void insert(int index,int element){
+		
+	}
+	
+	//头删
+	public void popFront(){
+		
+	}
+	
+	//尾删
+	public void popBack(){
+		
+	}
+	
+    public void erase(int index){
+	
+    }
+	
+	//查找
+	public int indexOf(int element){
+		
+	}
+	
+	//根据下标，获取元素
+    public int get(int index){
+	
+    }
+}
