99dc5385e5d96205fb7db923e929c7a957a50866
PrincessAlice <1337180988@qq.com>
2019-10-11 23:24:08 +0800

    提交

---
+++ Project/BookShelf.java
@@ -0,0 +1,32 @@
+package Databases;
+
+import Classes.Book;
+import exceptions.NoSuchBookException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookShelf {
+    //保存更多的书
+    //保存着书的集合
+    private List<Book> bookList = new ArrayList<>();
+
+    //饿汉模式
+    private static BookShelf instance = new BookShelf();
+    public static BookShelf getInstance() {
+        return instance;
+    }
+
+    public Book search(String ISBN) throws NoSuchBookException {
+        for(Book book :bookList){
+            if(book.is(ISBN)){
+                return book;
+            }
+        }
+        throw new NoSuchBookException(ISBN);
+    }
+
+    public void putBook(Book book) {
+        bookList.add(book);
+    }
+}
---
+++ Project/RecordShelf.java
@@ -0,0 +1,10 @@
+package Databases;
+
+import Classes.Record;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecordShelf {
+    private List<Record> recordList = new ArrayList<>();
+}
