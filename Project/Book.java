f9e1bfcc39542819eae03ba4cacafb33372b4690
PrincessAlice <1337180988@qq.com>
2019-10-11 23:20:00 +0800

    提交

---
+++ Project/Book.java
@@ -0,0 +1,40 @@
+package Classes;
+
+public class Book {
+
+    private String ISBN;
+    private String title;
+    private String author;
+    private double price;
+    //存量
+    private int currentCount;
+    //总量
+    private int totalCount;
+    //借阅次数
+    private int borrowedCount;
+
+    public Book(String ISBN,String title,String author,double price,int Count){
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.currentCount = Count;
+        this.totalCount = Count;
+        this.borrowedCount = 0;
+    }
+
+    public boolean is(String ISBN){
+        return this.ISBN.equals(ISBN);
+    }
+
+    public void increaseCount(int count) {
+        currentCount = count;   //剩下的书
+        totalCount += count;        //总数量
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+}
---
+++ Project/Record.java
@@ -0,0 +1,9 @@
+package Classes;
+
+import java.util.Date;
+
+public class Record {
+    private String userId;
+    private String bookISBN;
+    private Date borrowedAt;
+}
