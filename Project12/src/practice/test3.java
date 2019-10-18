7ab0f2072a34f8f27e5a30009c9e8b1e1df2d3a1
PrincessAlice <1337180988@qq.com>
2019-10-18 23:27:31 +0800

    提交

---
+++ Project12/src/practice/test3.java
@@ -0,0 +1,17 @@
+package practice;
+
+//打印金三角
+public class test3 {
+    public static void main(String[] args) {
+        int line = 10;
+        for(int x =0;x < line;x++){ //控制行数
+            for(int y = 0;y < line;y++){ //控制空格
+                System.out.print("");
+            }
+            for(int z = 0; z<x;z++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
