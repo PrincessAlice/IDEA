9cb04603d80f74106de040999d8eb8aa303cee90
PrincessAlice <1337180988@qq.com>
2019-11-07 00:21:34 +0800

    提交

---
+++ Project11/src/person.java
@@ -0,0 +1,23 @@
+package lianxi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class person {
+    private String name;
+    private String remarks;
+    private String tel;
+
+    public person(String name, String remarks, String tel){
+        this.name = name;
+        this.remarks = remarks;
+        this.tel = tel;
+    }
+
+    //输入多个电话
+    //public List<String> InputTel(String tel){
+     //   ArrayList<String> list = new ArrayList<>();
+   // }
+
+
+}
