fb0d4b18d7c04f1eb5442a8d1c914d83e6c50cbb
PrincessAlice <1337180988@qq.com>
2019-12-22 17:33:32 +0800

    提交

---
+++ Project11/src/practice/test17.java
@@ -0,0 +1,19 @@
+package practice;
+
+//求n阶方阵和
+import java.util.Scanner;
+
+public class test17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0, x;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                x = sc.nextInt();
+                ans += x;
+            }
+        }
+        System.out.println(ans);
+    }
+}
