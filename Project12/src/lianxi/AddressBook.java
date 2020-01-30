eb822f0aa31179f3d57b755ec0aad1a1f7015504
PrincessAlice <1337180988@qq.com>
2020-01-30 23:52:32 +0800

    提交

---
+++ Project12/src/lianxi/AddressBook.java
@@ -0,0 +1,18 @@
+package lianxi;
+
+import java.util.*;
+
+public class AddressBook {
+    //查找
+    Map<String,String> map = new HashMap<>();
+   // public static String selected(String name){}
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+      //  String s = selected(name);
+       // System.out.println(s);
+
+    }
+}
