ddc78aee415b3a088daa4f23c62f7d25dc39b5ee
PrincessAlice <1337180988@qq.com>
2020-01-31 00:05:58 +0800

    提交

---
+++ Project12/src/lianxi/User.java
@@ -0,0 +1,13 @@
+package lianxi;
+
+public class User {
+    private String name;
+    private String remarks;
+    private String tel;
+    public User(String name,String tel){
+        this.name = name;
+        this.remarks = remarks;
+        this.tel = tel;
+    }
+
+}
