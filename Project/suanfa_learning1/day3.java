24bcb92767568b2d3c5ab20df2e2a896e5aa096f
PrincessAlice <1337180988@qq.com>
2020-02-02 22:39:37 +0800

    提交

---
+++ Project/suanfa_learning1/day3.java
@@ -0,0 +1,10 @@
+public class day3 {
+    public void loop() {
+        int x = 10;
+        while (x) {
+            System.out.print("x minus one is " + (x - 1));
+            x -= 1;
+        }
+    }
+
+}
