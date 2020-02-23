841ac1769b9957efe6382aa9b040291bcf946c10
PrincessAlice <1337180988@qq.com>
2020-02-23 23:31:34 +0800

    提交

---
+++ Maven-test/src/main/java/task/DBInit.java
@@ -0,0 +1,29 @@
+package task;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Arrays;
+
+public class DBInit {
+    public static Connection {
+        Connection connection = null;
+        Statement statement = null;
+
+    }
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(readSQL()));
+        init();
+    }
+    /* *
+    *
+    * */
+
+    public static void close(Connection,Statement,){
+        try{
+
+        }catch (){
+
+        }
+    }
+}
