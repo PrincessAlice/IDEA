9672571d326e5f671275ed71a1225dee3f7ae656
PrincessAlice <1337180988@qq.com>
2020-03-18 22:38:49 +0800

    提交

---
+++ project00/servlet-blog/src/main/java/frank/exception/BaseException.java
@@ -0,0 +1,20 @@
+package frank.exception;
+
+public class BaseException extends RuntimeException{
+
+    private  String code;
+
+    private String message;
+
+    public BaseException(String code,String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+
+    public BaseException(String code,String message, Throwable cause) {
+        super(message, cause);
+        this.code = code;
+        this.message = message;
+    }
+}
