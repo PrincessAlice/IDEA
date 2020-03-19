0bcb07085bc641d0ac3b0093bdb45625b1bd2f42
PrincessAlice <1337180988@qq.com>
2020-03-19 23:21:56 +0800

    提交

---
+++ project00/servlet-blog/src/main/java/frank/model/Result.java
@@ -0,0 +1,19 @@
+package frank.model;
+
+public class Result {
+
+    //是否请求成功
+    private boolean Success;
+
+    public void setSuccess(boolean b) {
+    }
+
+    public void setCode(String s) {
+    }
+
+    public void setMessage(String 操作成功) {
+    }
+
+    public void setData(Object data) {
+    }
+}
