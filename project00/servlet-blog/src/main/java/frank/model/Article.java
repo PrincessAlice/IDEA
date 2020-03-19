305958aeb4c528141d4946cda1bd52536d48a08e
PrincessAlice <1337180988@qq.com>
2020-03-19 23:06:44 +0800

    提交

---
+++ project00/servlet-blog/src/main/java/frank/model/Article.java
@@ -0,0 +1,20 @@
+package frank.model;
+
+import java.util.Date;
+
+public class Article {
+    public void setId(int id) {
+    }
+
+    public void setTitle(String title) {
+    }
+
+    public void setContent(String content) {
+    }
+
+    public void setUserId(Integer id) {
+    }
+
+    public void setCreateTime(Date create_time) {
+    }
+}
