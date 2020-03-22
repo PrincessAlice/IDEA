1b88ac60842d1dc9bc5eabbd62291eb4bd4e925d
PrincessAlice <1337180988@qq.com>
2020-03-22 21:33:14 +0800

    提交

---
+++ project00/servlet-blog/src/main/java/frank/util/DBUtil.java
@@ -0,0 +1,15 @@
+package frank.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBUtil {
+
+    public static Connection getConnection(){
+
+    }
+    public static void close(Connection connection, PreparedStatement ps, ResultSet resultSet){
+
+    }
+}
